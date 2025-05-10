b018ed38b06aad51beda84c039dba241d2835f93
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-05-10 13:47:53 +0100

    Usability improvements

--- firmware/src/MouseController.h
+++ firmware/src/MouseController.h
@@ -3,28 +3,81 @@
 #include "HID-Project.h"
 
 class MouseController {
+private:
+    enum class Direction {
+        NONE,
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+    
+    Direction currentDirection = Direction::NONE;
+    unsigned long lastMoveTime = 0;
+    const unsigned long MOVE_INTERVAL = 500; // 500ms between moves (slower)
+    const int MOVE_AMOUNT = 20;  // Smaller movement amount
+
 public:
     void begin() {
         Mouse.begin();
     }
 
     void moveUp() {
-        Mouse.move(0, -40);
+        if (currentDirection == Direction::UP) {
+            currentDirection = Direction::NONE;
+        } else {
+            currentDirection = Direction::UP;
+        }
     }
 
     void moveDown() {
-        Mouse.move(0, 40);
+        if (currentDirection == Direction::DOWN) {
+            currentDirection = Direction::NONE;
+        } else {
+            currentDirection = Direction::DOWN;
+        }
     }
 
     void moveLeft() {
-        Mouse.move(-40, 0);
+        if (currentDirection == Direction::LEFT) {
+            currentDirection = Direction::NONE;
+        } else {
+            currentDirection = Direction::LEFT;
+        }
     }
 
     void moveRight() {
-        Mouse.move(40, 0);
+        if (currentDirection == Direction::RIGHT) {
+            currentDirection = Direction::NONE;
+        } else {
+            currentDirection = Direction::RIGHT;
+        }
     }
 
     void click() {
         Mouse.click();
+    }
+
+    void update() {
+        unsigned long currentTime = millis();
+        if (currentTime - lastMoveTime >= MOVE_INTERVAL) {
+            switch (currentDirection) {
+                case Direction::UP:
+                    Mouse.move(0, -MOVE_AMOUNT);
+                    break;
+                case Direction::DOWN:
+                    Mouse.move(0, MOVE_AMOUNT);
+                    break;
+                case Direction::LEFT:
+                    Mouse.move(-MOVE_AMOUNT, 0);
+                    break;
+                case Direction::RIGHT:
+                    Mouse.move(MOVE_AMOUNT, 0);
+                    break;
+                case Direction::NONE:
+                    break;
+            }
+            lastMoveTime = currentTime;
+        }
     }
 }; 
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -18,6 +18,9 @@
 #define RIGHT_BUTTON "B813"
 #define CLICK_BUTTON "B11"
 
+// Mode switch
+#define MODE_SWITCH "MODE"
+
 // Keyboard controls
 #define SELECT_ALL "KSA"
 #define COPY "KC"
@@ -25,26 +28,28 @@
 #define DELETE "KD"
 
 // BIOS navigation keys
-#define BIOS_UP "KBU"    // Up arrow
-#define BIOS_DOWN "KBD"  // Down arrow
-#define BIOS_LEFT "KBL"  // Left arrow
-#define BIOS_RIGHT "KBR" // Right arrow
-#define BIOS_ENTER "KBE" // Enter
-#define BIOS_ESC "KBX"   // Escape
-#define BIOS_F2 "KF2"    // F2 (common BIOS key)
-#define BIOS_F10 "KF10"  // F10 (common BIOS key)
-#define BIOS_F12 "KF12"  // F12 (common BIOS key)
+#define BIOS_UP "KBU"       // Up arrow
+#define BIOS_DOWN "KBD"     // Down arrow
+#define BIOS_LEFT "KBL"     // Left arrow
+#define BIOS_RIGHT "KBR"    // Right arrow
+#define BIOS_ENTER "KBE"    // Enter
+#define BIOS_ESC "KBX"      // Escape
+#define BIOS_F2 "KF2"       // F2 (common BIOS key)
+#define BIOS_F10 "KF10"     // F10 (common BIOS key)
+#define BIOS_F12 "KF12"     // F12 (common BIOS key)
 #define BIOS_DELETE "KBDEL" // Delete key for BIOS entry
-#define BIOS_LOOP "KBL" // Loop key for BIOS entry
+#define BIOS_LOOP "KBL"     // Loop key for BIOS entry
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 MouseController mouse;
 KeyboardController keyboard;
 
+bool keyboardMode = false;
+
 void setup()
 {
     Serial.begin(9600);
-    
+
     // start ble
     if (!ble.begin(true))
     {
@@ -80,98 +85,122 @@ void setup()
     keyboard.begin();
 }
 
+void processMouseCommands(String command)
+{
+    if (command.indexOf(UP_BUTTON) >= 0)
+    {
+        mouse.moveUp();
+    }
+    else if (command.indexOf(DOWN_BUTTON) >= 0)
+    {
+        mouse.moveDown();
+    }
+    else if (command.indexOf(LEFT_BUTTON) >= 0)
+    {
+        mouse.moveLeft();
+    }
+    else if (command.indexOf(RIGHT_BUTTON) >= 0)
+    {
+        mouse.moveRight();
+    }
+    else if (command.indexOf(CLICK_BUTTON) >= 0)
+    {
+        mouse.click();
+    }
+}
+
+void processKeyboardCommands(String command)
+{
+    if (command.indexOf(SELECT_ALL) >= 0)
+    {
+        keyboard.selectAll();
+    }
+    else if (command.indexOf(COPY) >= 0)
+    {
+        keyboard.copy();
+    }
+    else if (command.indexOf(PASTE) >= 0)
+    {
+        keyboard.paste();
+    }
+    else if (command.indexOf(DELETE) >= 0)
+    {
+        keyboard.deleteKey();
+    }
+    else if (command.indexOf(BIOS_UP) >= 0)
+    {
+        keyboard.upArrow();
+    }
+    else if (command.indexOf(BIOS_DOWN) >= 0)
+    {
+        keyboard.downArrow();
+    }
+    else if (command.indexOf(BIOS_LEFT) >= 0)
+    {
+        keyboard.leftArrow();
+    }
+    else if (command.indexOf(BIOS_RIGHT) >= 0)
+    {
+        keyboard.rightArrow();
+    }
+    else if (command.indexOf(BIOS_ENTER) >= 0)
+    {
+        keyboard.sendEnter();
+    }
+    else if (command.indexOf(BIOS_DELETE) >= 0)
+    {
+        keyboard.sendKey(KEY_DELETE);
+    }
+    else if (command.indexOf(BIOS_ESC) >= 0)
+    {
+        keyboard.sendKey(KEY_ESC);
+    }
+    else if (command.indexOf(BIOS_F2) >= 0)
+    {
+        keyboard.sendKey(KEY_F2);
+    }
+    else if (command.indexOf(BIOS_F10) >= 0)
+    {
+        keyboard.sendKey(KEY_F10);
+    }
+    else if (command.indexOf(BIOS_F12) >= 0)
+    {
+        keyboard.sendKey(KEY_F12);
+    }
+    else if (command.indexOf(BIOS_LOOP) >= 0)
+    {
+        keyboard.msiBiosLoop();
+    }
+    else
+    {
+        keyboard.sendString(command);
+    }
+}
+
 void loop()
 {
+    if (!keyboardMode)
+    {
+        mouse.update();
+    }
+
     if (ble.available())
     {
         String command = ble.readStringUntil('\n');
         command.trim();
         Serial.println(command);
 
-        if (command.indexOf(UP_BUTTON) >= 0)
+        if (command.indexOf(MODE_SWITCH) >= 0)
         {
-            mouse.moveUp();
+            keyboardMode = !keyboardMode;
         }
-        else if (command.indexOf(DOWN_BUTTON) >= 0)
+        else if (keyboardMode)
         {
-            mouse.moveDown();
-        }
-        else if (command.indexOf(LEFT_BUTTON) >= 0)
-        {
-            mouse.moveLeft();
-        }
-        else if (command.indexOf(RIGHT_BUTTON) >= 0)
-        {
-            mouse.moveRight();
-        }
-        else if (command.indexOf(CLICK_BUTTON) >= 0)
-        {
-            mouse.click();
-        }
-        else if (command.indexOf(SELECT_ALL) >= 0)
-        {
-            keyboard.selectAll();
-        }
-        else if (command.indexOf(COPY) >= 0)
-        {
-            keyboard.copy();
-        }
-        else if (command.indexOf(PASTE) >= 0)
-        {
-            keyboard.paste();
-        }
-        else if (command.indexOf(DELETE) >= 0)
-        {
-            keyboard.deleteKey();
-        }
-        // BIOS navigation handlers
-        else if (command.indexOf(BIOS_UP) >= 0)
-        {
-            keyboard.upArrow();
-        }
-        else if (command.indexOf(BIOS_DOWN) >= 0)
-        {
-            keyboard.downArrow();
-        }
-        else if (command.indexOf(BIOS_LEFT) >= 0)
-        {
-            keyboard.leftArrow();
-        }
-        else if (command.indexOf(BIOS_RIGHT) >= 0)
-        {
-            keyboard.rightArrow();
-        }
-        else if (command.indexOf(BIOS_ENTER) >= 0)
-        {
-            keyboard.sendEnter();
-        }
-        else if (command.indexOf(BIOS_DELETE) >= 0)
-        {
-            keyboard.sendKey(KEY_DELETE);
-        }
-        else if (command.indexOf(BIOS_ESC) >= 0)
-        {
-            keyboard.sendKey(KEY_ESC);
-        }
-        else if (command.indexOf(BIOS_F2) >= 0)
-        {
-            keyboard.sendKey(KEY_F2);
-        }
-        else if (command.indexOf(BIOS_F10) >= 0)
-        {
-            keyboard.sendKey(KEY_F10);
-        }
-        else if (command.indexOf(BIOS_F12) >= 0)
-        {
-            keyboard.sendKey(KEY_F12);
-        }
-        else if (command.indexOf(BIOS_LOOP) >= 0)
-        {
-            keyboard.msiBiosLoop();
+            processKeyboardCommands(command);
         }
         else
         {
-            keyboard.sendString(command);
+            processMouseCommands(command);
         }
     }
 }
