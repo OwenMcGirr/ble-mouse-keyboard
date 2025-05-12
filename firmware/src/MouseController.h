32c3d123ad211607b5b80a21ee967f9199cd46db
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-05-12 20:26:52 +0100

    feat: enhance mouse controls with explicit modes and right-click - Add separate small (20px) and large (80px) movement commands - Add diagonal movement support in both sizes - Add explicit mode switching (MOUSE_MOUSE and MODE_KEYBOARD) - Add right-click functionality - Improve command naming for better clarity - Remove continuous movement automation

--- firmware/src/MouseController.h
+++ firmware/src/MouseController.h
@@ -4,80 +4,89 @@
 
 class MouseController {
 private:
-    enum class Direction {
-        NONE,
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT
-    };
-    
-    Direction currentDirection = Direction::NONE;
-    unsigned long lastMoveTime = 0;
-    const unsigned long MOVE_INTERVAL = 500; // 500ms between moves (slower)
-    const int MOVE_AMOUNT = 20;  // Smaller movement amount
+    const int SMALL_MOVE = 20;
+    const int LARGE_MOVE = 80;
 
 public:
     void begin() {
         Mouse.begin();
     }
 
-    void moveUp() {
-        if (currentDirection == Direction::UP) {
-            currentDirection = Direction::NONE;
-        } else {
-            currentDirection = Direction::UP;
-        }
+    // Small movements
+    void moveUpSmall() {
+        Mouse.move(0, -SMALL_MOVE);
     }
 
-    void moveDown() {
-        if (currentDirection == Direction::DOWN) {
-            currentDirection = Direction::NONE;
-        } else {
-            currentDirection = Direction::DOWN;
-        }
+    void moveDownSmall() {
+        Mouse.move(0, SMALL_MOVE);
     }
 
-    void moveLeft() {
-        if (currentDirection == Direction::LEFT) {
-            currentDirection = Direction::NONE;
-        } else {
-            currentDirection = Direction::LEFT;
-        }
+    void moveLeftSmall() {
+        Mouse.move(-SMALL_MOVE, 0);
     }
 
-    void moveRight() {
-        if (currentDirection == Direction::RIGHT) {
-            currentDirection = Direction::NONE;
-        } else {
-            currentDirection = Direction::RIGHT;
-        }
+    void moveRightSmall() {
+        Mouse.move(SMALL_MOVE, 0);
+    }
+
+    void moveUpLeftSmall() {
+        Mouse.move(-SMALL_MOVE, -SMALL_MOVE);
+    }
+
+    void moveUpRightSmall() {
+        Mouse.move(SMALL_MOVE, -SMALL_MOVE);
+    }
+
+    void moveDownLeftSmall() {
+        Mouse.move(-SMALL_MOVE, SMALL_MOVE);
+    }
+
+    void moveDownRightSmall() {
+        Mouse.move(SMALL_MOVE, SMALL_MOVE);
+    }
+
+    // Large movements
+    void moveUpLarge() {
+        Mouse.move(0, -LARGE_MOVE);
+    }
+
+    void moveDownLarge() {
+        Mouse.move(0, LARGE_MOVE);
+    }
+
+    void moveLeftLarge() {
+        Mouse.move(-LARGE_MOVE, 0);
+    }
+
+    void moveRightLarge() {
+        Mouse.move(LARGE_MOVE, 0);
+    }
+
+    void moveUpLeftLarge() {
+        Mouse.move(-LARGE_MOVE, -LARGE_MOVE);
+    }
+
+    void moveUpRightLarge() {
+        Mouse.move(LARGE_MOVE, -LARGE_MOVE);
+    }
+
+    void moveDownLeftLarge() {
+        Mouse.move(-LARGE_MOVE, LARGE_MOVE);
+    }
+
+    void moveDownRightLarge() {
+        Mouse.move(LARGE_MOVE, LARGE_MOVE);
     }
 
     void click() {
         Mouse.click();
     }
 
+    void rightClick() {
+        Mouse.click(MOUSE_RIGHT);
+    }
+
     void update() {
-        unsigned long currentTime = millis();
-        if (currentTime - lastMoveTime >= MOVE_INTERVAL) {
-            switch (currentDirection) {
-                case Direction::UP:
-                    Mouse.move(0, -MOVE_AMOUNT);
-                    break;
-                case Direction::DOWN:
-                    Mouse.move(0, MOVE_AMOUNT);
-                    break;
-                case Direction::LEFT:
-                    Mouse.move(-MOVE_AMOUNT, 0);
-                    break;
-                case Direction::RIGHT:
-                    Mouse.move(MOVE_AMOUNT, 0);
-                    break;
-                case Direction::NONE:
-                    break;
-            }
-            lastMoveTime = currentTime;
-        }
+        // No longer needed as we removed automation
     }
 }; 
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,34 +11,52 @@
 #define BLUEFRUIT_SPI_IRQ 7
 #define BLUEFRUIT_SPI_RST 4 // Optional but recommended, set to -1 if unused
 
-// Mouse controls
-#define UP_BUTTON "B516"
-#define DOWN_BUTTON "B615"
-#define LEFT_BUTTON "B714"
-#define RIGHT_BUTTON "B813"
-#define CLICK_BUTTON "B11"
+// Mode commands
+#define MODE_MOUSE "MODE_MOUSE"
+#define MODE_KEYBOARD "MODE_KEYBOARD"
 
-// Mode switch
-#define MODE_SWITCH "MODE"
+// Mouse controls - Small movements (20px)
+#define MOUSE_UP_SMALL "MOUSE_UP_20"
+#define MOUSE_DOWN_SMALL "MOUSE_DOWN_20"
+#define MOUSE_LEFT_SMALL "MOUSE_LEFT_20"
+#define MOUSE_RIGHT_SMALL "MOUSE_RIGHT_20"
+#define MOUSE_UP_LEFT_SMALL "MOUSE_UP_LEFT_20"
+#define MOUSE_UP_RIGHT_SMALL "MOUSE_UP_RIGHT_20"
+#define MOUSE_DOWN_LEFT_SMALL "MOUSE_DOWN_LEFT_20"
+#define MOUSE_DOWN_RIGHT_SMALL "MOUSE_DOWN_RIGHT_20"
+
+// Mouse controls - Large movements (80px)
+#define MOUSE_UP_LARGE "MOUSE_UP_80"
+#define MOUSE_DOWN_LARGE "MOUSE_DOWN_80"
+#define MOUSE_LEFT_LARGE "MOUSE_LEFT_80"
+#define MOUSE_RIGHT_LARGE "MOUSE_RIGHT_80"
+#define MOUSE_UP_LEFT_LARGE "MOUSE_UP_LEFT_80"
+#define MOUSE_UP_RIGHT_LARGE "MOUSE_UP_RIGHT_80"
+#define MOUSE_DOWN_LEFT_LARGE "MOUSE_DOWN_LEFT_80"
+#define MOUSE_DOWN_RIGHT_LARGE "MOUSE_DOWN_RIGHT_80"
+
+// Mouse click controls
+#define MOUSE_LEFT_CLICK "MOUSE_LEFT_CLICK"
+#define MOUSE_RIGHT_CLICK "MOUSE_RIGHT_CLICK"
 
 // Keyboard controls
-#define SELECT_ALL "KSA"
-#define COPY "KC"
-#define PASTE "KP"
-#define DELETE "KD"
+#define KEYBOARD_SELECT_ALL "KEYBOARD_SELECT_ALL"
+#define KEYBOARD_COPY "KEYBOARD_COPY"
+#define KEYBOARD_PASTE "KEYBOARD_PASTE"
+#define KEYBOARD_DELETE "KEYBOARD_DELETE"
 
 // BIOS navigation keys
-#define BIOS_UP "KBU"       // Up arrow
-#define BIOS_DOWN "KBD"     // Down arrow
-#define BIOS_LEFT "KBL"     // Left arrow
-#define BIOS_RIGHT "KBR"    // Right arrow
-#define BIOS_ENTER "KBE"    // Enter
-#define BIOS_ESC "KBX"      // Escape
-#define BIOS_F2 "KF2"       // F2 (common BIOS key)
-#define BIOS_F10 "KF10"     // F10 (common BIOS key)
-#define BIOS_F12 "KF12"     // F12 (common BIOS key)
-#define BIOS_DELETE "KBDEL" // Delete key for BIOS entry
-#define BIOS_LOOP "KBL"     // Loop key for BIOS entry
+#define BIOS_UP "BIOS_UP"
+#define BIOS_DOWN "BIOS_DOWN"
+#define BIOS_LEFT "BIOS_LEFT"
+#define BIOS_RIGHT "BIOS_RIGHT"
+#define BIOS_ENTER "BIOS_ENTER"
+#define BIOS_ESC "BIOS_ESC"
+#define BIOS_F2 "BIOS_F2"
+#define BIOS_F10 "BIOS_F10"
+#define BIOS_F12 "BIOS_F12"
+#define BIOS_DELETE "BIOS_DELETE"
+#define BIOS_LOOP "BIOS_LOOP"
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 MouseController mouse;
@@ -87,43 +105,95 @@ void setup()
 
 void processMouseCommands(String command)
 {
-    if (command.indexOf(UP_BUTTON) >= 0)
+    if (command.indexOf(MOUSE_UP_SMALL) >= 0)
     {
-        mouse.moveUp();
+        mouse.moveUpSmall();
     }
-    else if (command.indexOf(DOWN_BUTTON) >= 0)
+    else if (command.indexOf(MOUSE_DOWN_SMALL) >= 0)
     {
-        mouse.moveDown();
+        mouse.moveDownSmall();
     }
-    else if (command.indexOf(LEFT_BUTTON) >= 0)
+    else if (command.indexOf(MOUSE_LEFT_SMALL) >= 0)
     {
-        mouse.moveLeft();
+        mouse.moveLeftSmall();
     }
-    else if (command.indexOf(RIGHT_BUTTON) >= 0)
+    else if (command.indexOf(MOUSE_RIGHT_SMALL) >= 0)
     {
-        mouse.moveRight();
+        mouse.moveRightSmall();
     }
-    else if (command.indexOf(CLICK_BUTTON) >= 0)
+    else if (command.indexOf(MOUSE_UP_LEFT_SMALL) >= 0)
+    {
+        mouse.moveUpLeftSmall();
+    }
+    else if (command.indexOf(MOUSE_UP_RIGHT_SMALL) >= 0)
+    {
+        mouse.moveUpRightSmall();
+    }
+    else if (command.indexOf(MOUSE_DOWN_LEFT_SMALL) >= 0)
+    {
+        mouse.moveDownLeftSmall();
+    }
+    else if (command.indexOf(MOUSE_DOWN_RIGHT_SMALL) >= 0)
+    {
+        mouse.moveDownRightSmall();
+    }
+    else if (command.indexOf(MOUSE_UP_LARGE) >= 0)
+    {
+        mouse.moveUpLarge();
+    }
+    else if (command.indexOf(MOUSE_DOWN_LARGE) >= 0)
+    {
+        mouse.moveDownLarge();
+    }
+    else if (command.indexOf(MOUSE_LEFT_LARGE) >= 0)
+    {
+        mouse.moveLeftLarge();
+    }
+    else if (command.indexOf(MOUSE_RIGHT_LARGE) >= 0)
+    {
+        mouse.moveRightLarge();
+    }
+    else if (command.indexOf(MOUSE_UP_LEFT_LARGE) >= 0)
+    {
+        mouse.moveUpLeftLarge();
+    }
+    else if (command.indexOf(MOUSE_UP_RIGHT_LARGE) >= 0)
+    {
+        mouse.moveUpRightLarge();
+    }
+    else if (command.indexOf(MOUSE_DOWN_LEFT_LARGE) >= 0)
+    {
+        mouse.moveDownLeftLarge();
+    }
+    else if (command.indexOf(MOUSE_DOWN_RIGHT_LARGE) >= 0)
+    {
+        mouse.moveDownRightLarge();
+    }
+    else if (command.indexOf(MOUSE_LEFT_CLICK) >= 0)
     {
         mouse.click();
+    }
+    else if (command.indexOf(MOUSE_RIGHT_CLICK) >= 0)
+    {
+        mouse.rightClick();
     }
 }
 
 void processKeyboardCommands(String command)
 {
-    if (command.indexOf(SELECT_ALL) >= 0)
+    if (command.indexOf(KEYBOARD_SELECT_ALL) >= 0)
     {
         keyboard.selectAll();
     }
-    else if (command.indexOf(COPY) >= 0)
+    else if (command.indexOf(KEYBOARD_COPY) >= 0)
     {
         keyboard.copy();
     }
-    else if (command.indexOf(PASTE) >= 0)
+    else if (command.indexOf(KEYBOARD_PASTE) >= 0)
     {
         keyboard.paste();
     }
-    else if (command.indexOf(DELETE) >= 0)
+    else if (command.indexOf(KEYBOARD_DELETE) >= 0)
     {
         keyboard.deleteKey();
     }
@@ -179,21 +249,24 @@ void processKeyboardCommands(String command)
 
 void loop()
 {
-    if (!keyboardMode)
-    {
-        mouse.update();
-    }
-
     if (ble.available())
     {
         String command = ble.readStringUntil('\n');
         command.trim();
         Serial.println(command);
 
-        if (command.indexOf(MODE_SWITCH) >= 0)
+        // Process mode commands first
+        if (command.indexOf(MODE_MOUSE) >= 0)
         {
-            keyboardMode = !keyboardMode;
+            keyboardMode = false;
+            Serial.println("Switched to Mouse Mode");
         }
+        else if (command.indexOf(MODE_KEYBOARD) >= 0)
+        {
+            keyboardMode = true;
+            Serial.println("Switched to Keyboard Mode");
+        }
+        // Then process the appropriate commands based on current mode
         else if (keyboardMode)
         {
             processKeyboardCommands(command);
